74788e7e7b43c8b304e1a9ebe63d90fe83638c07
MyYeonbi <yeonbi_kim@icloud.com>
2024-10-06 23:20:22 +0900

    -

--- src/main/java/hello/core/beanFind/ApplicationContextInfoTest.java
+++ src/main/java/hello/core/beanFind/ApplicationContextInfoTest.java
@@ -1,8 +1,7 @@
 package hello.core.beanFind;
 
 import hello.core.AppConfig;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
+
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,8 +11,7 @@ public class ApplicationContextInfoTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
-    @Test
-    @DisplayName("애플리케이션 빈 출력하기")
+ 
     void findApplicationBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
