91a3a15fc2c7527e520104020df6e833f296266a
MyYeonbi <yeonbi_kim@icloud.com>
2024-10-29 08:14:02 +0900

    -

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,36 +3,19 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.AppConfig;
 import hello.core.member.MemberServiceImpl;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.MessageSourceResolvable;
-import org.springframework.context.NoSuchMessageException;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.core.ResolvableType;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.Resource;
 
-import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
 
 public class MemberApp {
 
     public static void main(String[] args) {
        MemberService memberService = new MemberServiceImpl();
-       Member member = new Member(1L, "memberA", Grade.VIP);
+       Member member = new Member(1L, "member A", Grade.VIP);
        memberService.join(member);
 
        Member findMember = memberService.findMember(1L);
-       System.out.println("new member = " + member.getName());
+        System.out.println("new member = " + member.getName());
         System.out.println("findMember = " + findMember.getName());
+
     }
 }
