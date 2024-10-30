b13607bd0cbbb4da0f5380dc2beea2e43880c33b
MyYeonbi <yeonbi_kim@icloud.com>
2024-10-30 15:28:45 +0900

    -

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,9 +1,10 @@
 package hello.core.member;
 
-import hello.core.AppConfig;
-import org.assertj.core.api.Assertions;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MemberServiceTest {
 
@@ -13,9 +14,11 @@ public class MemberServiceTest {
     void join() {
         //given
         Member member = new Member(1L, "member A", Grade.VIP);
+
         //when
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
+
         //then
         Assertions.assertThat(member).isEqualTo(findMember);
 
