28bfc269f078866f6c1f6fe9b5b07cedb637bd34
MyYeonbi <yeonbi_kim@icloud.com>
2024-10-31 02:01:19 +0900

    -

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -8,29 +8,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OrderServiceTest {
 
-    MemberService memberService ;
-    OrderService orderService ;
+   MemberService memberService = new MemberServiceImpl();
+   OrderService orderService = new OrderServiceImpl();
 
-    @BeforeEach
-    public void beforeEach() {
-        AppConfig appConfig = new AppConfig();
-        memberService = appConfig.memberService();
-        orderService = appConfig.orderService();
-
-    }
-
-    @Test
+   @Test
     void createOrder() {
-        Long memberId = 1L;
+       long memberId = 1L;
        Member member = new Member(memberId, "memberA", Grade.VIP);
-        memberService.join(member);
+       memberService.join(member);
 
-        Order order = orderService.createOrder(memberId,"itemA", 10000);
-        Assertions.assertEquals(1000, order.getDiscountPrice());
-
-    }
+       Order order = orderService.createOrder(memberId, "itemA", 10000);
+       Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+   }
 }
