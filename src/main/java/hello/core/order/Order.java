15fa03a699c8805480f61eed2eaa95d9a3e85eb4
MyYeonbi <yeonbi_kim@icloud.com>
2024-10-31 00:10:15 +0900

    -

--- src/main/java/hello/core/order/Order.java
+++ src/main/java/hello/core/order/Order.java
@@ -7,12 +7,13 @@ public class Order {
     private int itemPrice;
     private int discountPrice;
 
-    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice){
         this.memberId = memberId;
         this.itemName = itemName;
         this.itemPrice = itemPrice;
         this.discountPrice = discountPrice;
     }
+
     public int calculatePrice() {
         return itemPrice - discountPrice;
     }
@@ -21,43 +22,25 @@ public class Order {
         return memberId;
     }
 
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
-
     public String getItemName() {
         return itemName;
-    }
-
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
     }
 
     public int getItemPrice() {
         return itemPrice;
     }
 
-    public void setItemPrice(int itemPrice) {
-        this.itemPrice = itemPrice;
-    }
-
-    public int getDiscountPrice() {
+    public getDiscountPrice() {
         return discountPrice;
-    }
-
-    public void setDiscountPrice(int discountPrice) {
-        this.discountPrice = discountPrice;
     }
 
     @Override
     public String toString() {
         return "Order{" +
                 "memberId=" + memberId +
-                ", itemName='" + itemName + '\'' +
+                ", itemName=" + itemName + '\'' +
                 ", itemPrice=" + itemPrice +
                 ", discountPrice=" + discountPrice +
                 '}';
-
-
     }
 }
