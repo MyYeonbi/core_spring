ce4f99736f35b2050dbda332790cbd2bf6092272
MyYeonbi <yeonbi_kim@icloud.com>
2024-11-05 10:29:24 +0900

    **사용 클래스 - MemberApp**

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -5,14 +5,14 @@ import hello.core.member.*;
 
 public class MemberApp {
 
-    public static void main(String[] args) {
-       MemberService memberService = new MemberServiceImpl(new MemoryMemberRepository());
-       Member member = new Member(1L, "member A", Grade.VIP);
+   public static void main(String[] args) {
+       AppConfig appConfig = new AppConfig();
+       MemberService memberService = appConfig.memberService();
+       Member member = new Member(1L, "memberA", Grade.VIP);
        memberService.join(member);
 
        Member findMember = memberService.findMember(1L);
-        System.out.println("new member = " + member.getName());
-        System.out.println("findMember = " + findMember.getName());
-
-    }
+       System.out.println("new member = " + member.getName());
+       System.out.println("find member = " + findMember.getName());
+   }
 }
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -11,19 +11,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class OrderApp {
     public static void main(String[] args) {
-        //MemberRepository와 DiscountPolicy의 구현체 생성
-        MemberRepository memberRepository = new MemoryMemberRepository();
-        DiscountPolicy discountPolicy = new FixDiscountPolicy();
-
-        // OrderServiceImpl에 적절한 인자 전달
-        OrderService orderService = new OrderServiceImpl(memberRepository, discountPolicy);
-
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
-        // MemberService.join(member); //memberService라는 변수는 존재하지 않으므로, 이 줄은 제거해야 한다.
+        memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId,"itemA", 10000);
 
         System.out.println("order = " + order);
 
