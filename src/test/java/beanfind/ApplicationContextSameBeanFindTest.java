e29c4c30743c0267569419c866f8a37ebbfb3db4
MyYeonbi <yeonbi_kim@icloud.com>
2024-11-25 22:18:16 +0900

    :sparkles: Feat : 모든 빈 출력 / 애플리케이션 빈 출력

--- src/test/java/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/beanfind/ApplicationContextSameBeanFindTest.java
@@ -16,28 +16,5 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationContextSameBeanFindTest {
 
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
 
-    @Test
-    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다.")
-    void findBeanByTypeDuplicate() {
-        //MemberRepository bean = ac.getBean(MemberRepository.class);
-        assertThrows(NoUniqueBeanDefinitionException.class, () -> ac.getBean(MemberRepository.class));
-    }
-
-    @Test
-    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 빈 이름을 지정하면 된다.")
-    void findBeanName() {
-        MemberRepository memberRepository = ac.getBean(MemberRepository.class);
-        assertThat(memberRepository).isInstanceOf(MemberRepository.class);
-    }
-
-    @Test
-    @DisplayName("특저 타입을 모두 조회하기")
-    void findAllBeanByType() {
-        Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
-        for (String key : beansOfType.keySet()) {
-            System.out.println("key: " + key + ", value: " + beansOfType.get(key));
-        }
-    }
 }
