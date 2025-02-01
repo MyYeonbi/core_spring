ae2bf3330525dff78bd1d618c42d442f578bd829
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-01 20:39:21 +0900

    🎨 `AppConfig`에서 회원 저장소를 메서드로 분리하여 유지보수성 개선
    - `new MemoryMemberRepository()`를 직접 주입하던 방식을 변경
    - `memberRepository()` 메서드를 통해 저장소를 주입하도록 수정
    
    이제 저장소 변경이 필요할 경우 `AppConfig`에서 `memberRepository()`만 수정하면
    `MemberServiceImpl`을 수정하지 않고도 쉽게 변경할 수 있습니다.

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -3,11 +3,12 @@ package hello.core;
 import hello.core.discount.OrderService;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
 
 public class AppConfig {
 
     public MemberService memberService () {
-        return new MemberServiceImpl();
+        return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
 
