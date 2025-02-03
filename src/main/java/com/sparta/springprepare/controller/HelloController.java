6a296123c114eab7057b47cc7cce678881122f2e
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-03 22:20:45 +0900

    🍰 @RestController를 사용하여 @ResponseBody 동작 적용
    	•	@RestController는 내부적으로 @ResponseBody를 포함하여 JSON 또는 문자열 데이터를 직접 반환하는 역할을 함.
    	•	@ResponseBody는 컨트롤러의 반환 값을 HTTP 응답 본문(body)로 변환하여 클라이언트에 전달하는 역할을 수행.
    	•	기존의 @Controller와 달리, @RestController를 사용하면 모든 메서드가 자동으로 @ResponseBody를 적용받음.
    	•	해당 변경을 통해 RESTful API의 응답 처리 방식을 명확하게 이해하고 활용할 수 있도록 구현.

--- src/main/java/com/sparta/springprepare/controller/HelloController.java
+++ src/main/java/com/sparta/springprepare/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/api/hello")
+    @ResoponseBody
     public String hello() {
         return "Hello world";
     }
