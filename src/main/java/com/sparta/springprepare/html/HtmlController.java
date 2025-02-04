a150b7370b23a50fbf8e115ff23e1109f2b892f8
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-04 10:53:15 +0900

    🍊 Thymeleaf 기반 정적 페이지 서빙을 위한 HtmlController 추가
    •	@Controller를 사용하여 HTML 파일을 반환하는 컨트롤러 추가.
    •	@GetMapping("/static-hello")을 통해 /static-hello 경로에서 hello.html을 렌더링하도록 구현.
    •	return "hello";를 사용하여 ViewResolver가 src/main/resources/templates/hello.html을 찾도록 설정.
    	•	Spring Boot에서 기본적으로 Thymeleaf 템플릿 엔진을 사용할 경우, 정적 HTML 파일은 templates/ 폴더 내에 위치해야 함.

---
+++ src/main/java/com/sparta/springprepare/html/HtmlController.java
@@ -0,0 +1,13 @@
+package com.sparta.springprepare.html;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HtmlController {
+
+    @GetMapping("/static-hello")
+    public String hello() {
+        return "hello.html"
+    }
+
+}
