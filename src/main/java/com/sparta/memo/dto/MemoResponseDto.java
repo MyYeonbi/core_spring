9cb194fc72e41f376f078fa71528a7cadd4c0b11
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-05 11:48:54 +0900

    🍀 MemoResponseDto 추가
    - 클라이언트에게 응답할 DTO 클래스 생성
    - id, username, contents 필드 포함
    - Lombok @Getter 적용하여 getter 자동 생성

---
+++ src/main/java/com/sparta/memo/dto/MemoResponseDto.java
@@ -0,0 +1,12 @@
+package com.sparta.memo.dto;
+
+
+
+import lombok.Getter;
+
+@Getter
+public class MemoResponseDto {
+    private Long id;
+    private String username;
+    private String contents;
+}
