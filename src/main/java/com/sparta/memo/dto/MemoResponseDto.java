6f66012ec90cfdf494a6c0ea12b41388eb28dd72
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-05 12:15:27 +0900

    🍀 MemoResponseDto에 Memo 객체를 활용한 생성자 추가
    - Memo 엔티티를 매개변수로 받아 MemoResponseDto 객체를 생성하는 생성자 추가
    - id, username 필드를 Memo 엔티티에서 가져와 초기화

--- src/main/java/com/sparta/memo/dto/MemoResponseDto.java
+++ src/main/java/com/sparta/memo/dto/MemoResponseDto.java
@@ -2,6 +2,7 @@ package com.sparta.memo.dto;
 
 
 
+import com.sparta.memo.entity.Memo;
 import lombok.Getter;
 
 @Getter
@@ -9,4 +10,9 @@ public class MemoResponseDto {
     private Long id;
     private String username;
     private String contents;
+
+    public MemoResponseDto(Memo memo) {
+        this.id = memo.getId();
+        this.username = memo.getUsername();
+    }
 }
