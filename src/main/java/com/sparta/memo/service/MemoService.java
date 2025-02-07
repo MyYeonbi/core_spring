a1d824d8ddceb89f9c6c344365d55dc90dcac059
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-07 10:49:10 +0900

    🌺 ApplicationContext에서 Bean 클래스 타입으로 의존성 주입 방식 변경
    - MemoRepository를 ApplicationContext를 통해 이름("memoRepository")으로 가져오던 방식을 클래스 타입으로 변경
    - context.getBean(MemoRepository.class) 방식으로 더 안전하고 명확하게 Bean을 가져옴
    - Bean 조회 방식 개선으로 코드 가독성과 안정성 향상

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -19,8 +19,11 @@ public class MemoService {
 
     public MemoService(ApplicationContext context) {
         // 1.'Bean' 이름으로 가져오기
-        MemoRepository memoRepository = (MemoRepository) context.getBean("memoRepository");
-       this.memoRepository = memoRepository;
+        //MemoRepository memoRepository = (MemoRepository) context.getBean("memoRepository");
+
+        // 2. 'Bean' 클래스 형식으로 가져오기.
+        MemoRepository memoRepository = context.getBean(MemoRepository.class);
+        this.memoRepository = memoRepository;
     }
 
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
