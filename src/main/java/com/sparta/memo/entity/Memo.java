75a6733a06701618f3f8a4676bcb1667bef165f8
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-07 21:26:59 +0900

    📖 Memo 엔티티 클래스 생성 및 JPA 매핑 설정
    - @Table 어노테이션으로 'memo' 테이블과 매핑
    - @Id 및 @GeneratedValue(strategy = GenerationType.IDENTITY)로 기본 키 설정
    - @Column 어노테이션을 사용해 'username'과 'contents' 컬럼 매핑
    - Lombok 어노테이션(@Getter, @Setter, @NoArgsConstructor) 추가로 코드 간소화

--- src/main/java/com/sparta/memo/entity/Memo.java
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -2,16 +2,24 @@ package com.sparta.memo.entity;
 
 
 import com.sparta.memo.dto.MemoRequestDto;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Table(name = "memo") // 매핑할 테이블의 이름을 저장.
 @NoArgsConstructor
 public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "username", nullable = false)
     private String username;
+
+    @Column(name = "contents", nullable = false, length = 500)
     private String contents;
 
     public Memo(MemoRequestDto requestDto) {
