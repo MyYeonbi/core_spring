afaf0ebdd5166b4ac2550f21cb98fdcd26459c0b
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-07 20:47:47 +0900

    🐘 Gradle 종속성 추가: JPA, MySQL, Thymeleaf 및 Lombok 설정
    - JPA(Spring Data JPA) 의존성 추가로 ORM 기능 사용 준비
    - MySQL 커넥터 의존성 추가로 MySQL 데이터베이스 연동 지원
    - Thymeleaf 의존성 추가로 템플릿 엔진 설정
    - Lombok 의존성 추가로 코드 간소화 및 생산성 향상
    - Spring Web 및 테스트 종속성 유지

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -10,6 +10,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -17,7 +18,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
-@Component
+@Repository
 public class MemoRepository {
 
     private final JdbcTemplate jdbcTemplate;
