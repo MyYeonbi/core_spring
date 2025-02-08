919aa4a44c5c26ee904ea4528f5a8063dd0ae05a
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-08 21:02:55 +0900

    🐰 UserController 추가 및 사용자 관련 기능 구현
    - `/user/login-page`: 로그인 페이지로 이동
    - `/user/signup`: 회원가입 페이지로 이동
    - `/user/kakao/callback`: 카카오 로그인 콜백 처리 메서드 추가
    - `UserService`, `FolderService`, `KakaoService` 의존성 주입

--- src/main/java/com/sparta/memo/Controller/UserController.java
+++ src/main/java/com/sparta/memo/Controller/UserController.java
@@ -1,10 +1,35 @@
 package com.sparta.memo.Controller;
 
 import ch.qos.logback.core.model.Model;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class UserController {
+
+    private final UserService userService;
+    private final FolderService folderService;
+    private final KakaoService kakaoService;
+
+
+    @GetMapping("/user/login-page")
+    public String loginpage() {return "login";}
+
+
+    @GetMapping("/user/signup")
+    public String signupPage() { return "signup";}
 
 
 
@@ -25,5 +50,10 @@ public class UserController {
         model.addAttribute("folders", folderService.getFolders(userDetails.getUser()));
 
         return "index :: fragment";
+    }
+
+    @GetMapping("/user/kakao/callback")
+    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) {
+
     }
 }
