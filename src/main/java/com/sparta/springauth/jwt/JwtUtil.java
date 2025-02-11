edbcfe3397cda2be7413214d81989746ce7af276
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-11 19:52:41 +0900

    🍋 JWT에서 사용자 정보(Claims) 추출 기능 추가

--- src/main/java/com/sparta/springauth/jwt/JwtUtil.java
+++ src/main/java/com/sparta/springauth/jwt/JwtUtil.java
@@ -95,6 +95,10 @@ public class JwtUtil {
             }
             return false;
         }
+        
     //JWT에서 사용자 정보 가져오기
+        public Claims getUserInfoFromToken(String token) {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+        }
 
 }
