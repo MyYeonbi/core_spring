2c881d1e7d6b3c350a9e1ddc6349b378903fff06
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-11 19:06:11 +0900

    🍋 JWT 토큰 생성 기능 구현

--- src/main/java/com/sparta/springauth/jwt/JwtUtil.java
+++ src/main/java/com/sparta/springauth/jwt/JwtUtil.java
@@ -38,6 +38,18 @@ public class JwtUtil {
     }
 
     //JWT 데이터
+    // 토큰 생성
+    public String createToken(String username, UserRoleEnum role) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(username) // 사용자 식별자값(ID)
+                        .claim(AUTHORIZATION_KEY, role) // 사용자 권한
+                        .setExpiration(new Date(date.getTime() + TOKEN_TIME)) // 만료 시간
+                        .setIssuedAt(date) // 발급일
+                        .signWith(key, signatureAlgorithm) // 암호화 알고리즘
+                        .compact…
 
     //JWT 생성
 
